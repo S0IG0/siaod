b7a4852926a83d5725d18ad886ee6f6e179dc80b
Даниил <dchibitok@yandex.ru>
2022-03-18 10:44:49 +0300

    Библиотека для работы с длинной арифметикой

---
+++ main/Big_integer.h
@@ -0,0 +1,70 @@
+#ifndef BIG_INTEGER_H
+#define BIG_INTEGER_H
+
+#include <vector>
+#include <string>
+#include <ostream>
+#include <iomanip>
+#include <sstream>
+
+class big_integer {
+	// основание системы счисления (1 000 000 000)
+	static const int BASE = 100000000000;
+
+	// внутреннее хранилище числа
+	std::vector<int> _digits;
+
+	// знак числа
+	bool _is_negative;
+
+	void _remove_leading_zeros();
+	void _shift_right();
+
+public:
+	// класс-исключение, бросаемое при делении на ноль
+	class divide_by_zero : public std::exception {  };
+
+	big_integer();
+	big_integer(std::string);
+	big_integer(signed char);
+	big_integer(unsigned char);
+	big_integer(signed short);
+	big_integer(unsigned short);
+	big_integer(signed int);
+	big_integer(unsigned int);
+	big_integer(signed long);
+	big_integer(unsigned long);
+	big_integer(signed long long);
+	big_integer(unsigned long long);
+
+	friend std::ostream& operator <<(std::ostream&, const big_integer&);
+	operator std::string() const;
+	const big_integer operator +() const;
+	const big_integer operator -() const;
+	const big_integer operator ++();
+	const big_integer operator ++(int);
+	const big_integer operator --();
+	const big_integer operator --(int);
+	friend bool operator ==(const big_integer&, const big_integer&);
+	friend bool operator <(const big_integer&, const big_integer&);
+	friend bool operator !=(const big_integer&, const big_integer&);
+	friend bool operator <=(const big_integer&, const big_integer&);
+	friend bool operator >(const big_integer&, const big_integer&);
+	friend bool operator >=(const big_integer&, const big_integer&);
+	friend const big_integer operator +(big_integer, const big_integer&);
+	big_integer& operator +=(const big_integer&);
+	friend const big_integer operator -(big_integer, const big_integer&);
+	big_integer& operator -=(const big_integer&);
+	friend const big_integer operator *(const big_integer&, const big_integer&);
+	big_integer& operator *=(const big_integer&);
+	friend const big_integer operator /(const big_integer&, const big_integer&);
+	big_integer& operator /=(const big_integer&);
+	friend const big_integer operator %(const big_integer&, const big_integer&);
+	big_integer& operator %=(const big_integer&);
+
+	bool odd() const;
+	bool even() const;
+	const big_integer pow(big_integer) const;
+};
+
+#endif // BIG_INTEGER_H
